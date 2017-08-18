2441faeeae8880046b2687450ac12e46d84a080a
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-19 00:44:54 +0300

    add function for comparing points for sorting in clockwise order

--- GridExtractor.h
+++ GridExtractor.h
@@ -36,6 +36,32 @@ private:
         return line1Length > line2Length;
     }
 
+    static bool comparePointsClockwise(cv::Point a, cv::Point b, cv::Point center)
+    {
+        if (a.x - center.x >= 0 && b.x - center.x < 0)
+            return true;
+        if (a.x - center.x < 0 && b.x - center.x >= 0)
+            return false;
+        if (a.x - center.x == 0 && b.x - center.x == 0) {
+            if (a.y - center.y >= 0 || b.y - center.y >= 0)
+                return a.y > b.y;
+            return b.y > a.y;
+        }
+
+        // compute the cross product of vectors (center -> a) x (center -> b)
+        int det = (a.x - center.x) * (b.y - center.y) - (b.x - center.x) * (a.y - center.y);
+        if (det < 0)
+            return true;
+        if (det > 0)
+            return false;
+
+        // points a and b are on the same line from the center
+        // check which point is closer to the center
+        int d1 = (a.x - center.x) * (a.x - center.x) + (a.y - center.y) * (a.y - center.y);
+        int d2 = (b.x - center.x) * (b.x - center.x) + (b.y - center.y) * (b.y - center.y);
+        return d1 > d2;
+    }
+
     std::vector<cv::Vec4i> filterSimmilar(std::vector<cv::Vec4i> lines, double angleThresh, double lengthThresh) const;
 
     int orientation(const cv::Point &p, const cv::Point &q, const cv::Point &r) const;
