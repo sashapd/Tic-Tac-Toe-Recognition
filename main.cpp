dbda43b6f5b905659fcdee91ebce09e943473237
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-21 02:37:10 +0300

    Show grid values in command line

--- main.cpp
+++ main.cpp
@@ -4,61 +4,35 @@
 #include "opencv2/imgproc.hpp"
 #include <opencv2/core/mat.hpp>
 #include "GridExtractor.h"
-#include "GameState.h"
-#include "GameAutomate.h"
-
-void drawState(GameState state)
-{
-	std::cout << std::endl;
-	for (int r = 0; r < GameState::kRowCount; ++r)
-	{
-		for (int c = 0; c < GameState::kColumnsCount; ++c)
-		{
-			if (state.getCell(r, c) == X)
-			{
-				std::cout << 'x';
-			} else if (state.getCell(r, c) == O)
-			{
-				std::cout << 'o';
-			} else
-			{
-				std::cout << ' ';
-			}
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
-}
 
 int main() {
+    cv::Mat im = cv::imread("hoax4.jpg");
+    cv::Mat image;
+    cv::resize(im, image, cv::Size(), 0.5, 0.5);
 
-	GameState game_state;
+    GridExtractor extractor(image);
+    extractor.extractGrid();
 
-	drawState(game_state);
+    if (!extractor.hasFoundGrid()) {
+        std::cout << "Grid not found" << std::endl;
+    } else {
+        Grid grid = extractor.getGrid();
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                Cell c = grid.getCellValue(j, i);
+                if(c == O) {
+                    std::cout << "O" << " ";
+                } else if(c == X) {
+                    std::cout << "X" << " ";
+                } else {
+                    std::cout << " " << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+        cv::imshow("image", grid.getImage());
+        cv::waitKey(0);
+    }
 
-	GameAutomate automate(O);
-
-	while (!game_state.isOsWin() && !game_state.isXsWin() && !game_state.isDraw())
-	{
-		std::cout << "Enter x position (row col): ";
-		int row;
-		int col;
-		std::cin >> row;
-		std::cin >> col;
-		if (game_state.getCell(row, col) != NONE)
-		{
-			continue;
-		}
-		game_state.setCell(row, col, X);
-		game_state = automate.makeTurn(game_state);
-		drawState(game_state);
-	}
-
-	drawState(game_state);
-	std::cout << "is draw: " << game_state.isDraw() << std::endl;
-	std::cout << "is x's win " << game_state.isXsWin() << std::endl;
-	std::cout << "is o's win" << game_state.isOsWin() << std::endl;
-
-	system("Pause");
     return 0;
 }
