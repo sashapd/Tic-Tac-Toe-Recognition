bf790839059e07f64c2ab84119426934a07302dc
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-21 02:35:15 +0300

    Implement Grid class

--- Grid.cpp
+++ Grid.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "Grid.h"
+#include "CellClassifier.h"
+
+Grid::Grid(){}
+
+Grid::Grid(cv::Mat gridImage) {
+    mGridImage = gridImage;
+}
+
+Grid::Grid(const Grid &grid) {
+    mGridImage = grid.mGridImage;
+}
+
+Grid &Grid::operator=(const Grid &grid) {
+    mGridImage = mGridImage;
+    return *this;
+}
+
+cv::Mat Grid::getImage() {
+    return mGridImage;
+}
+
+Cell Grid::getCellValue(int x, int y) {
+    cv::Mat cellImg = getCellImage(x, y);
+
+    CellClassifier classifier(cellImg);
+
+    Cell value = classifier.getCellValue();
+
+    return value;
+}
+
+cv::Mat Grid::getCellImage(int x, int y) {
+    int cellHeigh = mGridImage.rows / 3;
+    int cellWidth = mGridImage.cols / 3;
+
+    cv::Rect roiRect(x * cellWidth, y * cellHeigh, cellWidth, cellHeigh);
+    cv::Mat cellRoi = mGridImage(roiRect);
+
+    return cellRoi;
+}
--- Grid.h
+++ Grid.h
@@ -7,13 +7,26 @@
 
 
 #include <opencv2/core/mat.hpp>
+#include "Cell.h"
 
 class Grid {
 public:
+    Grid();
+
     Grid(cv::Mat gridImage);
+
+    Grid(const Grid &grid);
+
+    Grid &operator=(const Grid &grid);
+
+    cv::Mat getImage();
+
+    Cell getCellValue(int x, int y);
 
 private:
     cv::Mat mGridImage;
+
+    cv::Mat getCellImage(int x, int y);
 };
 
 
