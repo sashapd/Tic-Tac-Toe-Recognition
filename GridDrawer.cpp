4a2e64a42bb4b10c08023fa573850b8fcd5b29ed
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-21 15:53:39 +0300

    Add GridDrawer class

---
+++ GridDrawer.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Oleksandr Padko on 8/21/17.
+//
+
+#include <opencv2/imgproc.hpp>
+#include "GridDrawer.h"
+
+GridDrawer::GridDrawer(Grid grid): crossColor(255, 0, 0), circleColor(0, 0, 255) {
+    mGrid = grid;
+}
+
+void GridDrawer::drawCircle(int x, int y) {
+    cv::Mat cellImg = mGrid.getCellImage(x, y);
+    cv::Point center(cellImg.cols / 2, cellImg.rows / 2);
+    cv::circle(cellImg, center, cellImg.cols / 2, circleColor, 3);
+}
+
+void GridDrawer::drawCross(int x, int y) {
+    cv::Mat cellImg = mGrid.getCellImage(x, y);
+    int offset = cellImg.cols / 10;
+    cv::Point p1(offset, offset), p2(cellImg.cols - offset, cellImg.rows - offset);
+    cv::Point p3(offset, cellImg.rows - offset), p4(cellImg.cols - offset, offset);
+    cv::line(cellImg, p1, p2, crossColor, 3);
+    cv::line(cellImg, p3, p4, crossColor, 3);
+}
+
+void GridDrawer::drawWinnerLine(int x1, int y1, int x2, int y2, Cell winner) {
+    cv::Mat gridImage = mGrid.getImage();
+    int cellWidth = gridImage.cols / 3;
+    int cellHeigh = gridImage.rows / 3;
+    cv::Point p1(x1 * cellWidth + cellWidth/2, y1 * cellHeigh + cellHeigh/2);
+    cv::Point p2(x2 * cellWidth + cellWidth/2, y2 * cellHeigh + cellHeigh/2);
+    cv::Scalar color;
+    if(winner == X) {
+        color = crossColor;
+    } else {
+        color = circleColor;
+    }
+    cv::line(gridImage, p1, p2, color, 3);
+}
---
+++ GridDrawer.h
@@ -0,0 +1,24 @@
+//
+// Created by Oleksandr Padko on 8/21/17.
+//
+
+#ifndef TICTACTOERECOGNITION_GRIDDRAWER_H
+#define TICTACTOERECOGNITION_GRIDDRAWER_H
+
+
+#include "Grid.h"
+
+class GridDrawer {
+public:
+    GridDrawer(Grid grid);
+    void drawCircle(int x, int y);
+    void drawCross(int x, int y);
+    void drawWinnerLine(int x1, int y1, int x2, int y2, Cell winner);
+private:
+    Grid mGrid;
+    const cv::Scalar crossColor;
+    const cv::Scalar circleColor;
+};
+
+
+#endif //TICTACTOERECOGNITION_GRIDDRAWER_H
