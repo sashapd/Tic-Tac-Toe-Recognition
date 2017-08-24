f425b721509f17074d32852580febcdb4f529cf3
Maksym Tsizh <zzviri@gmail.com>
2017-08-25 00:14:56 +0300

    Fully operational AI, added win situation check

--- Source.cpp
+++ Source.cpp
@@ -16,7 +16,7 @@ int whatturnisit(vector<int> a)
 	}
 	return a.size() - count;
 }
-int threepositionscheck(int b, int c, int d, int a) {
+int nextstepwincheck(int b, int c, int d, int a) {
 	int winpossition = -1;
 	if ((tttvector[d] * tttvector[b] * tttvector[c] == 18) && (a == 3)) {
 		if (tttvector[b] == 2) {
@@ -42,34 +42,104 @@ int threepositionscheck(int b, int c, int d, int a) {
 	}
 	return winpossition;
 }
-int posswin(int a){
+int gameovercheck(int b, int c, int d) {
+	int winplayer = -1;
+	if (tttvector[d] * tttvector[b] * tttvector[c] == 27) {
+		winplayer = 3;
+	}
+	if (tttvector[d] * tttvector[b] * tttvector[c] == 125) {
+		winplayer = 5;
+	}
+	return winplayer;
+}
+int nextstepwinposs(int a){
 	int winpos = -1;
-	winpos = threepositionscheck(0, 1, 2, a);
+	winpos = nextstepwincheck(0, 1, 2, a);
 	if (winpos < 0) {
-	    winpos = threepositionscheck(3, 4, 5, a);
+	    winpos = nextstepwincheck(3, 4, 5, a);
 	    if (winpos < 0) {
-		   	winpos = threepositionscheck(3, 4, 5, a);
-		    if (winpos < 0) {
-			    winpos = threepositionscheck(6, 7, 8, a);
+		 
+			    winpos = nextstepwincheck(6, 7, 8, a);
 			    if (winpos < 0) {
-				    winpos = threepositionscheck(0, 3, 6, a);
+				    winpos = nextstepwincheck(0, 3, 6, a);
 				    if (winpos < 0) {
-					    winpos = threepositionscheck(1, 4, 7, a);
+					    winpos = nextstepwincheck(1, 4, 7, a);
 				        if (winpos < 0) {
-					        winpos = threepositionscheck(2, 5, 8, a);
+					        winpos = nextstepwincheck(2, 5, 8, a);
 					        if (winpos < 0) {
-						        winpos = threepositionscheck(0, 4, 8, a);
+						        winpos = nextstepwincheck(0, 4, 8, a);
 						        if (winpos < 0) {
-									winpos = threepositionscheck(2, 4, 6, a);
+									winpos = nextstepwincheck(2, 4, 6, a);
 								}
 							}
 						}
 					}
 				}
-			}
+			
 		}
 	}
 	return winpos;
+}
+// gameoverposs returns {-1,-1,-1,-1} if there is no win situation. If else
+// it will return {a,b,c,d} where a,b,c is winner coordinates and d is 3 or 5
+// which means X or O
+vector<int> gameoverposs(vector<int> a) {
+	vector <int> winvect = { -1,-1,-1 };
+	int winner = -1;
+	winner = gameovercheck(0, 1, 2);
+	if (winner < 0) {
+		winner = gameovercheck(3, 4, 5);
+		if (winner < 0) {
+			winner = gameovercheck(6, 7, 8);
+			if (winner < 0) {
+				winner = gameovercheck(0, 3, 6);
+				if (winner < 0) {
+					winner = gameovercheck(1, 4, 7);
+					if (winner < 0) {
+						winner = gameovercheck(2, 5, 8);
+						if (winner < 0) {
+							winner = gameovercheck(0, 4, 8);
+							if (winner < 0) {
+								winner = gameovercheck(2, 4, 6);
+								if (winner > 0){
+									winvect = { 2, 4, 6 };
+								}
+							}
+							else {
+								winvect = { 0, 4, 8 };
+							}
+						}
+						else {
+							winvect = { 2, 5, 8 };
+						}
+					}
+					else {
+						winvect = { 1, 4, 7 };
+					}
+				}
+				else {
+					winvect = { 0, 3, 6 };
+				}
+			}
+			else {
+				winvect = { 6, 7, 8 };
+			}
+		}
+		else {
+			winvect = { 3, 4, 5 };
+		}
+	}
+	else {
+		winvect = { 0, 1, 2 };
+	}
+	vector<int> retvect = { -1,-1,-1,-1 };
+	if (winner > 0) {
+		retvect[0] = winvect[0];
+		retvect[1] = winvect[1];
+		retvect[2] = winvect[2];
+		retvect[3] = winner;
+	}
+	return retvect;
 }
 int Make2(vector<int> a) {
 	if (a[4] == 2){
@@ -82,9 +152,12 @@ int Make2(vector<int> a) {
 	}
 }
 
-
+// Main function accepts the state-of-game in 1d vector with 9 elemnts
+// It transforms it into next-step-state-of-game vector. It will pick up by  
+// itself correct player (X or O) and do the step according 
+// to best possible strategy
 int main() {
-	tttvector = { 3, 5, 3, 2, 5, 2, 2, 2, 2};
+	tttvector = { 2, 3, 2, 5, 3, 5, 2, 2, 3};
 	for (int i=0; i<tttvector.size(); i++){
 		cout << tttvector[i] << ",";
 	}
@@ -111,27 +184,76 @@ int main() {
 		}
 	}
 	if (b == 3) {
-		if (posswin(3) > 0) {
-			tttvector[posswin(3)] = 5;
+		if (nextstepwinposs(3) > 0) {
+			tttvector[nextstepwinposs(3)] = 5;
 		}
 		else {
 			tttvector[Make2(tttvector)] = 5;
 		}
 	}
 	if (b == 4) {
-		if (posswin(3) > 0) {
-			tttvector[posswin(3)] = 3;
+		if (nextstepwinposs(3) > 0) {
+			tttvector[nextstepwinposs(3)] = 3;
 		}
 		else {
-			if (posswin(5) > 0) {
-				tttvector[posswin(5)] = 3;
+			if (nextstepwinposs(5) > 0) {
+				tttvector[nextstepwinposs(5)] = 3;
 			}
 			else {
 				if (tttvector[6] == 2) {
-					tttvector[6] == 3;
+					tttvector[6] = 3;
 				}
 				else {
-					tttvector[2] == 3;
+					tttvector[2] = 3;
+				}
+			}
+		}
+	}
+	if (b == 5) {
+		if (nextstepwinposs(5) > 0) {
+			tttvector[nextstepwinposs(5)] = 5;
+		}
+		else {
+			if (nextstepwinposs(3) > 0) {
+				tttvector[nextstepwinposs(3)] = 5;
+   			}?
+			else {
+				tttvector[Make2(tttvector)] = 5;
+			}
+		}
+	}
+	if ((b == 6) || (b==8)) {
+		if (nextstepwinposs(3) > 0) {
+			tttvector[nextstepwinposs(3)] = 3;
+		}
+		else {
+			if (nextstepwinposs(5) > 0) {
+				tttvector[nextstepwinposs(5)] = 3;
+			}
+			else {
+				for (int i = 0; i < tttvector.size(); i++) {
+					if (tttvector[i] == 2) {
+						tttvector[i] = 3;
+						break;
+					}
+				}
+			}
+		}
+	}
+	if (b == 7) {
+		if (nextstepwinposs(5) > 0) {
+			tttvector[nextstepwinposs(5)] = 5;
+		}
+		else {
+			if (nextstepwinposs(3) > 0) {
+				tttvector[nextstepwinposs(3)] = 5;
+			}
+			else {
+				for (int i = 0; i < tttvector.size(); i++) {
+					if (tttvector[i] == 2) {
+						tttvector[i] = 5;
+						break;
+					}
 				}
 			}
 		}
