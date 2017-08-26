cbd1b1e71456ede79013161568ab0319f5f9a2ed
Oleksandr Padko <sasha.pad@gmail.com>
2017-08-26 15:11:59 +0300

    Fix makeRandomMove

--- GameAutomate.cpp
+++ GameAutomate.cpp
@@ -113,7 +113,7 @@ void GameAutomate::makeRandomMove(GameState& game_state)
 			for (int j = 0; j < 3; j++)
 				if (skip-- <= 0 && game_state.getCell(i, j) == NONE)
 				{
-					game_state.setCell(j, i, role);
+					game_state.setCell(i, j, role);
 					return;
 				}
 	}
