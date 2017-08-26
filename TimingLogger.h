16afda16a702142de319a2916cd51d0b76f0f4ec
Oleh Bernov <oleh.bernov@gmail.com>
2017-08-26 16:44:58 +0300

    Add TimingLogger

---
+++ TimingLogger.h
@@ -0,0 +1,31 @@
+﻿#ifndef TICTACTOERECOGNITION_TIMING_LOGGER_H
+#define TICTACTOERECOGNITION_TIMING_LOGGER_H
+
+#include <string>
+#include <map>
+
+class TimingLogger
+{
+public:
+	static TimingLogger* getInstance()
+	{
+		if (!sInstance)
+			sInstance = new TimingLogger;
+		return sInstance;
+	}
+
+	void onTaskStart(std::string task_name);
+
+	void onTaskEnd(std::string task_name);
+
+	void enableLogger();
+
+	void dissableLogger();
+
+private:
+	static TimingLogger* sInstance;
+	bool mIsLoggerEnabled;
+	std::map<std::string, long> mTrackingTasks;
+};
+
+#endif TICTACTOERECOGNITION_TIMING_LOGGER_H
